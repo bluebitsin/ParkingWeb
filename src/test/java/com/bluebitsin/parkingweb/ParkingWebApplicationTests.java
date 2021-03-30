2234e9074ec11f6112f78d490d67592adeb4859e
thunderbee <mukeshinhonkong@gmail.com>
2021-03-30 20:14:55 +0530

    first commit

---
+++ src/test/java/com/bluebitsin/parkingweb/ParkingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluebitsin.parkingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
